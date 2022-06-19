49323002e031900e6984f78c5759d9157ebbbecb
Gagan <107067282+gagankrpandit@users.noreply.github.com>
2022-06-19 19:14:26 +0530

    Create product_numbers.py

---
+++ product_numbers.py
@@ -0,0 +1,11 @@
+from itertools import product
+
+
+num = input('enter some numbers separated with comma: ')
+num = num.split(',')
+product = 1
+
+for i in range(len(num)):
+    product = product * int(num[i])
+
+print(f'Product of numbers is: {product}')
