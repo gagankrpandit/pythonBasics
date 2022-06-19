3ac7694adf8c580379662356c2335906a2ebc4f0
Gagan <107067282+gagankrpandit@users.noreply.github.com>
2022-06-20 00:32:15 +0530

    Update sum_of_digits.py

--- sum_of_digits.py
+++ sum_of_digits.py
@@ -1,17 +1,15 @@
 # Python program to find the sum of the digits of an integer using while loop
 
-int = int(input('Enter a positive integer: '))
-
-if int < 0:
-    print('You provided a negative integer!')
-    int = int(input('Enter a positive integer: '))
-else:
-    pass
-
-digit1 = 0
-while int > 0: 
-    digit = int % 10 
-    digit1 = digit1 + digit
-    int = int // 10 
-print(f'The sum of numbers given in input is: {digit1}')
-    
+while True:
+    num = input('Enter a positive integer: ')
+    num = int(num)
+    if num < 0:
+        print('You provided a negative integer!')
+    else:
+        digit1 = 0
+        while num > 0:
+            digit = num % 10
+            digit1 = digit1 + digit
+            num = num // 10
+        print(f'The sum of numbers given in input is: {digit1}')
+        break
