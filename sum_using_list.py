10f581494ee89b6bc347799c98f27d77166e88c8
Gagan <107067282+gagankrpandit@users.noreply.github.com>
2022-06-20 22:52:00 +0530

    Create sum_using_list.py

---
+++ sum_using_list.py
@@ -0,0 +1,12 @@
+# Python program to display the sum of n numbers using a list
+
+sum = []
+num = int(input('how many numbers?: '))
+d = 0
+
+for i in range(num):
+    n = input(f'Enter number {i+1}: ')
+    n = int(n)
+    d = d + n
+sum.append(d)
+print(f'The sum of the number in the list is: {sum}')
