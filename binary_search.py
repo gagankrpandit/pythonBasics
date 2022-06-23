673e49647ca7d01feffffd3671ee0a9cc01cca98
Gagan <107067282+gagankrpandit@users.noreply.github.com>
2022-06-24 01:32:07 +0530

    Update binary_search.py

--- binary_search.py
+++ binary_search.py
@@ -1,28 +1,19 @@
 # Python program to implement binary search
 
-list = [54, 67, 85, 33, 92, 74]
-list.sort()
-print(f'Given list is: {list}')
+aList = [54, 67, 85, 33, 92, 74, 75, 35, 66, 32, 24]
+aList.sort()
+print(f'Given list is: {aList}')
 
 low = 0
-upp = len(list)
+upp = len(aList) - 1
 mid = (low + upp) // 2
 
-num = list.index(int(input('Enter number: ')))
+num = int(input(f'Enter number from list: '))
 
-while num != mid:
+while num != aList[mid]:
     mid = (low + upp) // 2
-    if num < mid:
-        upp = mid
-        mid = (low + upp) // 2
+    if num > aList[mid]:
+        low = mid + 1
     else:
-        if num > mid:
-            low = mid
-            mid = (low + upp) // 2
-print(f'found at {mid} index')
-
-
-
-
-
-
+        upp = mid - 1
+print(f'found at {mid} index position')
