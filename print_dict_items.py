525af54268d5c67884b240a941c1e2471b8533fc
Gagan <107067282+gagankrpandit@users.noreply.github.com>
2022-06-27 23:16:43 +0530

    Create print_dict_items.py

---
+++ print_dict_items.py
@@ -0,0 +1,13 @@
+# Python program to print all the items in a dictionary
+
+def func():
+    dict_ = {
+    'name' : 'gagan',
+    'car': 'altroz',
+    'age' : '29'
+    }
+
+    for k, v in dict_.items():
+        print(k, ':', v)
+
+func()
