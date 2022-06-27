c6921bc5c6aa6eff7c10223e9dcd87fb7742fadc
Gagan <107067282+gagankrpandit@users.noreply.github.com>
2022-06-27 23:55:46 +0530

    Create turtle.py

---
+++ turtle.py
@@ -0,0 +1,22 @@
+# Python program to draw a circle of squares using Turtle
+
+import turtle
+
+db = turtle.Screen()
+db.bgcolor('light green')
+db.title('Turtle')
+x = turtle.Turtle()
+
+def square(angle):
+    x.forward(100)
+    x.right(angle)
+    x.forward(100)
+    x.right(angle)
+    x.forward(100)
+    x.right(angle)
+    x.forward(100)
+    x.right(angle+10)
+
+for i in range(36):
+    square(90)
+
