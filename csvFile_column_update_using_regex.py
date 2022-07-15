b0e768dc50c86dba8d274f4853c25c0d2bc53355
Gagan <107067282+gagankrpandit@users.noreply.github.com>
2022-07-16 01:20:51 +0530

    Update csvFile_column_update_using_regex.py

--- csvFile_column_update_using_regex.py
+++ csvFile_column_update_using_regex.py
@@ -2,7 +2,8 @@ import time
 start = time.process_time()
 import re
 import pandas as pd
-data = pd.read_csv('consumer_complaints - Copy.csv',sep=',', error_bad_lines=False, index_col=False, dtype='unicode')
+# col_list = ['date_received']
+data = pd.read_csv('consumer_complaints - Copy.csv', sep=',', error_bad_lines=False, index_col=False, dtype='unicode')
 
 for i in range(len(data)):
     try:
@@ -17,6 +18,22 @@ for i in range(len(data)):
         data.to_csv('consumer_complaints - Copy.csv', index=False)
     except:
         pass
-
 print(f'Done! The loop ran {i} times!')
-print(time.process_time() - start)
+print(f'Time taken by first loop: {time.process_time() - start} sec.')
+print('************************')
+
+for i in range(len(data)):
+    try:
+        regex = re.compile(r'\d\d/\d\d/\d\d\d\d')
+        mo = regex.search(str(data.loc[i, 'date_sent_to_company']))
+        mogrp = mo.string
+        dd = mogrp[3:5]
+        mm = mogrp[:2]
+        yyyy = mogrp[-4:]
+        date_ = dd + '-' + mm + '-' + yyyy
+        data.loc[i, 'date_sent_to_company'] = date_
+        data.to_csv('consumer_complaints - Copy.csv', index=False)
+    except:
+        pass
+print(f'Done! The loop ran {i} times!')
+print(f'Time taken by second loop: {time.process_time() - start} sec.')
